dc14ceb44572f2efbaaa46c89788f5dfd3d1bfbf
강민성 <pfcskms1997@naver.com>
2023-10-31 02:08:00 +0900

    upload YoutubeAdBlock Extension

---
+++ YoutubeAdBlock/scripts/content.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+	if (document.querySelectorAll('.ad-showing').length > 0) {
+		const video = document.querySelector('video');
+		if (video) {
+			video.currentTime = video.duration;
+		}
+	}
+}, 500);
